be967f8ae2af8bc4e777c5d82944a21e396235f9
dimitrilzk <dimitrilazuka95@gmail.com>
2024-01-18 21:44:02 +0100

    Aggiungere i file di progetto.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,7 @@
+﻿function addTodo() {
+    var td = $("#todoInput").val();
+    if (td != undefined) {
+        $(".lista-todo").append(`<h1 class="text-danger text-decoration-line-through">${td}</h1>`);
+        $("#todoInput").val("");
+    }
+}
