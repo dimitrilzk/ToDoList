fd8e6a3a230303e4fef5a5944925a47e719b8331
dimitrilzk <dimitrilazuka95@gmail.com>
2024-01-18 21:46:15 +0100

    init

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,7 +1,7 @@
 ﻿function addTodo() {
     var td = $("#todoInput").val();
     if (td != undefined) {
-        $(".lista-todo").append(`<h1 class="text-danger text-decoration-line-through">${td}</h1>`);
+        $(".lista-todo").append(`<h1 class="text-danger">${td}</h1><button>Delete</button>`);
         $("#todoInput").val("");
     }
 }
