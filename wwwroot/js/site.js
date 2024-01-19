2458c37f2c3f0236ea0c1291fe062d506aa8459f
dimitrilzk <dimitrilazuka95@gmail.com>
2024-01-19 19:26:49 +0100

    add light css

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,7 +1,9 @@
 ﻿function addTodo() {
     var td = $("#todoInput").val();
-    if (td != undefined) {
+    if (td != "") {
         $(".lista-todo").append(`<h1 class="text-danger">${td}</h1><button>Delete</button>`);
         $("#todoInput").val("");
+    } else {
+
     }
 }
