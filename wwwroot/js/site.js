416c43a168c8d66ba29af8d767751591b40c5bd8
dimitrilzk <dimitrilazuka95@gmail.com>
2024-01-19 22:06:12 +0100

    more css and delete todo

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,9 +1,23 @@
-﻿function addTodo() {
+﻿let todoCount = 0;
+function addTodo() {
     var td = $("#todoInput").val();
     if (td != "") {
-        $(".lista-todo").append(`<h1 class="text-danger">${td}</h1><button>Delete</button>`);
+        todoCount += 1;
+        $(".lista-todo").append(`
+        <div class="todoStyle">
+                <h1><span class="text-danger">${todoCount})</span> ${td}</h1>
+                <div>
+                    <button class="btn btn-warning ">Edit</button>
+                    <button onclick="deleteTodo()" class="btn btn-danger ">Delete</button>
+                </div>
+            </div>
+        `);
         $("#todoInput").val("");
     } else {
 
     }
+}
+
+function deleteTodo() {
+    $(".todoStyle").addClass("doneTodo");
 }
